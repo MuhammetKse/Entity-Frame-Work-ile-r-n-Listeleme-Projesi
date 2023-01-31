7795e057dd6e983b46318d791c86cb957f07469c
Muhammet Köse <umutkose3444@gmail.com>
2023-01-31 17:26:04 +0300

    Add project files.

---
+++ entityframeworkkullanımı/Form1.Designer.cs
@@ -0,0 +1,193 @@
+﻿namespace entityframeworkkullanımı
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.listBox1 = new System.Windows.Forms.ListBox();
+            this.btngetir = new System.Windows.Forms.Button();
+            this.listBox2 = new System.Windows.Forms.ListBox();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.button1 = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.listBox3 = new System.Windows.Forms.ListBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.txtmax = new System.Windows.Forms.TextBox();
+            this.label3 = new System.Windows.Forms.Label();
+            this.txtmin = new System.Windows.Forms.TextBox();
+            this.button2 = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // listBox1
+            // 
+            this.listBox1.FormattingEnabled = true;
+            this.listBox1.ItemHeight = 16;
+            this.listBox1.Location = new System.Drawing.Point(12, 176);
+            this.listBox1.Name = "listBox1";
+            this.listBox1.Size = new System.Drawing.Size(120, 260);
+            this.listBox1.TabIndex = 0;
+            this.listBox1.SelectedIndexChanged += new System.EventHandler(this.listBox1_SelectedIndexChanged);
+            // 
+            // btngetir
+            // 
+            this.btngetir.Location = new System.Drawing.Point(12, 132);
+            this.btngetir.Name = "btngetir";
+            this.btngetir.Size = new System.Drawing.Size(121, 38);
+            this.btngetir.TabIndex = 1;
+            this.btngetir.Text = "Getir";
+            this.btngetir.UseVisualStyleBackColor = true;
+            this.btngetir.Click += new System.EventHandler(this.btngetir_Click);
+            // 
+            // listBox2
+            // 
+            this.listBox2.FormattingEnabled = true;
+            this.listBox2.ItemHeight = 16;
+            this.listBox2.Location = new System.Drawing.Point(209, 176);
+            this.listBox2.Name = "listBox2";
+            this.listBox2.Size = new System.Drawing.Size(233, 260);
+            this.listBox2.TabIndex = 2;
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(288, 148);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(154, 22);
+            this.textBox1.TabIndex = 3;
+            this.textBox1.TextChanged += new System.EventHandler(this.textBox1_TextChanged);
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(288, 104);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(154, 38);
+            this.button1.TabIndex = 4;
+            this.button1.Text = "Verileri Getir";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(206, 151);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(76, 16);
+            this.label1.TabIndex = 5;
+            this.label1.Text = "Renk giriniz";
+            // 
+            // listBox3
+            // 
+            this.listBox3.FormattingEnabled = true;
+            this.listBox3.ItemHeight = 16;
+            this.listBox3.Location = new System.Drawing.Point(508, 178);
+            this.listBox3.Name = "listBox3";
+            this.listBox3.Size = new System.Drawing.Size(233, 260);
+            this.listBox3.TabIndex = 6;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(549, 157);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(32, 16);
+            this.label2.TabIndex = 8;
+            this.label2.Text = "Max";
+            // 
+            // txtmax
+            // 
+            this.txtmax.Location = new System.Drawing.Point(587, 151);
+            this.txtmax.Name = "txtmax";
+            this.txtmax.Size = new System.Drawing.Size(154, 22);
+            this.txtmax.TabIndex = 7;
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(553, 129);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(28, 16);
+            this.label3.TabIndex = 10;
+            this.label3.Text = "Min";
+            // 
+            // txtmin
+            // 
+            this.txtmin.Location = new System.Drawing.Point(587, 123);
+            this.txtmin.Name = "txtmin";
+            this.txtmin.Size = new System.Drawing.Size(154, 22);
+            this.txtmin.TabIndex = 9;
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(587, 79);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(154, 38);
+            this.button2.TabIndex = 11;
+            this.button2.Text = "Verileri Getir";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.txtmin);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.txtmax);
+            this.Controls.Add(this.listBox3);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.listBox2);
+            this.Controls.Add(this.btngetir);
+            this.Controls.Add(this.listBox1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.ListBox listBox1;
+        private System.Windows.Forms.Button btngetir;
+        private System.Windows.Forms.ListBox listBox2;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.ListBox listBox3;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.TextBox txtmax;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.TextBox txtmin;
+        private System.Windows.Forms.Button button2;
+    }
+}
+
---
+++ entityframeworkkullanımı/Form1.cs
@@ -0,0 +1,74 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Data.SqlClient;
+
+namespace entityframeworkkullanımı
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void btngetir_Click(object sender, EventArgs e)
+        {
+            AdventureWorks2019Entities db = new AdventureWorks2019Entities();
+            List<Product> urunlistesi = db.Products.Where(kayit=>kayit.Color=="kirmizi").ToList();  
+            foreach (var item in urunlistesi)
+            {
+                listBox1.Items.Add(item.Name);
+            }
+        }
+
+        private void listBox1_SelectedIndexChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            listBox2.Items.Clear(); 
+            string renk=textBox1.Text;
+            AdventureWorks2019Entities db = new AdventureWorks2019Entities();
+            List<Product> urunlistesi = db.Products.Where(kayit => kayit.Color == textBox1.Text).ToList(); 
+            foreach (var item in urunlistesi)
+            {
+                listBox2.Items.Add($"{item.Color} {item.Name}");
+            }
+        }
+
+        private void textBox1_TextChanged(object sender, EventArgs e)
+        {
+            
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            listBox3.Items.Clear();
+            decimal min, max;
+            min = Convert.ToDecimal(txtmin.Text);
+            max = Convert.ToDecimal(txtmax.Text);
+
+            AdventureWorks2019Entities db = new AdventureWorks2019Entities();
+            List<Product> urunlistesi =db.Products.Where(p => p.ListPrice>=min &&p.ListPrice<=max).ToList();
+            
+            foreach (var item in urunlistesi)
+            {
+                listBox3.Items.Add(item.ListPrice+" "+item.Color);
+            }
+        }
+    }
+}
